444dc6c424e149b608e609ff6ee6c436a3bbdc1f
maryam <mwaseem0488@gmail.com>
2025-04-19 11:11:50 +0500

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,55 @@
+# i=int(input("Enter A Number: "))
+# print("Even") if i%2==0 else print ("odd")
+
+# i=int(input("Number: "))
+# if i<0:
+#     print ("negative")
+# elif i>0:
+#     print("positive")
+# else:
+#     print("zero")    
+
+# age=int(input("Enter A Age: "))
+# if age<=10:
+#     print("Chlid")
+# elif age<=18:
+#     print("Teenage")
+# else:
+#     print("adult")
+
+
+# day=int(input("Enter Day Number"))
+
+# if day==1:
+#     print("Sunday")
+# elif day==2:
+#     print("mon")
+# elif day==3:
+#     print("tu")
+# elif day==4:
+#     print("wed")
+# elif day==5:
+#     print("th")
+# elif day==6:
+#     print("fri")
+# elif day>=7:
+#     print("Sat")
+# else:
+#     print("Enter 1 to 7 numbers only")
+
+# a=input("the first line of a nursery rhyme: ")
+# print(f"The {len(a)} of the string")
+# b=int(input("Enter starting number: "))
+# c=int(input("Enter ending number: "))
+# d=a[b:c]
+# # print(d)
+
+
+# Write a Python program that takes your name as input. If the length of your name is less than 5 characters, it asks for your surname, combines the two, and prints the result in uppercase. If the length of your name is 5 or more, it prints your name in lowercase.
+'''a=input("Enter Your Name: ")
+if len(a)<5:
+    b=input("Enter Your sure Name: ")
+    c=a+b
+    print(c.upper(c))
+else:
+    print(a.lower())'''
