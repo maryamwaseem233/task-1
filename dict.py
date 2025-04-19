a6622f386712251bd645926fb4293f0458e8dcf5
maryam <mwaseem0488@gmail.com>
2025-04-19 09:48:30 +0500

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,66 @@
+# Create an empty dictionary and print it.
+
+
+'''dict={
+
+ }
+print(dict)'''
+
+
+# Create a dictionary with the countries "Pakistan" and "India" as keys, and their respective capitals "Islamabad" and "New Delhi" as values. Print the capital of Pakistan from the dictionary.
+
+
+"""
+dict={
+    "Pakistan":"Islamabad",
+    "India":"new delhi"
+}
+print(dict["Pakistan"])
+"""
+
+# Add a new entry to the dictionary with the country "Italy" and its capital "Rome". Print the updated dictionary.
+
+"""dict={
+    "Pakistan":"Islamabad",
+    "India":"new delhi"
+}
+dict["italy"]="rome"
+print(dict)"""
+
+#Update the capital of Pakistan in the dictionary to "Karachi". Print the updated dictionary.
+"""dict={
+    "Pakistan":"Islamabad",
+    "India":"new delhi"
+}
+dict["Pakistan"]="Karachi"
+print(dict)
+"""
+#Create a dictionary with "Pakistan" and "India" as keys and their capitals as values. Use the keys() method to print all the keys in the dictionary.
+
+"""dict={
+    "Pakistan":"Islamabad",
+    "India":"new delhi"
+}
+print(dict.keys())
+"""
+
+
+# Attempt to print the capital of the United Kingdom from the dictionary. What will happen if the key doesn't exist?
+
+"""
+
+dict={
+    "Pakistan":"Islamabad",
+    "India":"new delhi"
+}
+print(dict["uk"])
+"""
+
+
+#Create a dictionary with "Pakistan" and "India" as keys and their capitals as values. Use the values() method to print all the values (capitals) in the dictionary.
+
+"""dict={
+    "Pakistan":"Islamabad",
+    "India":"new delhi"
+}
+print(dict.values())"""
