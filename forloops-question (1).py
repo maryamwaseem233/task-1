ac20efc225feca4b02c697df84af3406fbfea355
maryam <mwaseem0488@gmail.com>
2025-04-19 10:40:16 +0500

    Add files via upload

---
+++ forloops-question (1).py
@@ -0,0 +1,44 @@
+# 1-Print Numbers: Write a program to print numbers from 1 to 10 using a loop.
+"""
+for i in range(1,11):
+    print(i)
+    """
+
+ # 2- Even Numbers: Write a program to print all the even numbers between 1 and 20 using a loop.
+"""for i in range(2,21,2):
+    print(isi)"""
+
+# 3- Countdown: Write a program to print numbers from 10 to 1 in descending order using a loop.
+"""lis=[10,9,8,7,6,5,4,3,2,1]
+for j in lis:
+    print(j) """
+"""
+for j in range(10,0,-1):
+    print(j)
+"""
+# 4- Factorial: Write a program to find the factorial of a number using a loop.
+"""
+
+num = 5
+factorial = 1
+for i in range(1, num + 1):
+    factorial *= i
+print(f"The factorial of {num} is {factorial}")
+"""
+
+
+# 5- Write a program to calculate the sum of squares of the first 5 natural numbers using a loop.
+"""
+sum_of_sqr=0
+for n in range(0,6):
+    sum_of_sqr+=n**2
+print(sum_of_sqr)"""
+
+
+# 6- Reverse String: Write a program to reverse a given string using a loop.
+
+string= "Aptech"
+reverse_string = ""
+for characters in string:
+    reverse_string = characters + reverse_string 
+print(reverse_string)
