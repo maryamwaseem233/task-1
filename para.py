026097be8650110e63204054eccf022dd5a34974
maryam <mwaseem0488@gmail.com>
2025-04-19 10:41:30 +0500

    Add files via upload

---
+++ para.py
@@ -0,0 +1,13 @@
+def n(name, age, interests):
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+
+
+nam = input("Enter your name: ")
+ages = int(input("Enter your age: "))
+
+
+     
+n(nam, ages)
+
+
